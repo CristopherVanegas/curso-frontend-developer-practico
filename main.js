b92e3e829ce377e0ef50e2cf4f3cea00bbf84c49
CristopherVanegas <cristopher.platzi@gmail.com>
2022-09-05 07:25:46 -0500

    LOGIC TO TOGGLE HAMBURGUER MENU
    1. ADDED CONSTS WHICH SELECTS HTML ITEMS SUCH AS THE HAM-MENU ICON A
    ND THE MENU
    2. CREATED A EVENTLISTENER
    3. DEFINED THE FUNCTION WHICH TOGGLE
    
    LOGIC TO TOGGLE HAMBURGUER MENU
    
    1. ADDED CONSTS WHICH SELECTS HTML ITEMS SUCH AS THE HAM-MENU ICON AND THE MENU
    2. CREATED A EVENTLISTENER
    3. DEFINED THE FUNCTION WHICH TOGGLE INACTIVE .INACTIVE CLASS

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
 const menuEmail = document.querySelector('.navbar-right');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerIcon = document.querySelector('.hamburgerMenu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburguerIcon.addEventListener('click', toggleHamburguerMenu);
+
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
     // console.log("Click!");
 }
 
+function toggleHamburguerMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
+
 // console.log('JS working...');
+
