d99b30529709ae9742124341f63df818c2d26d12
CristopherVanegas <cristopher.platzi@gmail.com>
2022-09-12 07:01:14 -0500

    MERGED THE PRODUCT DETAIL
    1. CHANGED .navbar-right in 'menuEmail' querySelector
    2. ADDED menuCarritoIcon for listening and enabling aside for product-detail
    3. ADDED aside querySelector for product detail
    4. ADDED eventListener 'click' for menuCarritoIcon -> toggle carrito aside: active the product-detail window
    5. DELETED some useless comments

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 
-const menuEmail = document.querySelector('.navbar-right');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburguerIcon = document.querySelector('.hamburgerMenu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamburguerIcon.addEventListener('click', toggleHamburguerMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-    // console.log("Click!");
 }
 
 function toggleHamburguerMenu() {
     mobileMenu.classList.toggle('inactive');
 }
 
-// console.log('JS working...');
 
+function toggleCarritoAside() {
+    aside.classList.toggle('inactive');
+
+    if (mobileMenu.classList.contains('inactive')) {
+
+    }
+}
